63e33085a821ee8ce1a3a23f5576b18707004d3f
Erik Inkinen <erik.inkinen@erikinkinen.fi>
2024-12-16 02:19:42 +0200

    gdt: Simplify code
    We do not need to change the GDT. Therefore, a static GDT is enough.

--- src/arch/x86_64/gdt.c
+++ src/arch/x86_64/gdt.c
@@ -23,34 +23,15 @@ typedef struct {
 	uint64_t base;
 } __attribute__((packed)) gdtr;
 
-__attribute__((aligned(0x10))) static segment_descriptor gdt[6];
+__attribute__((aligned(0x10))) static segment_descriptor gdt[6] = {
+	{ 0, 0, 0, 0, 0, 0, 0 }, 			 // Null descriptor
+	{ 0xFFFF, 0, 0, 0x9A, 0xF, 0xA, 0 }, // Code segment
+	{ 0xFFFF, 0, 0, 0x92, 0xF, 0xC, 0 }, // Data segment
+	{ 0xFFFF, 0, 0, 0xFA, 0xF, 0xA, 0 }, // User mode code segment
+	{ 0xFFFF, 0, 0, 0xF2, 0xF, 0xC, 0 }, // User mode data segment
+};
 static gdtr _gdtr = { .limit = (uint16_t)sizeof(gdt) - 1,
 		      .base = (uintptr_t)gdt };
-
-/**
- * @brief Sets a descriptor in the Global Descriptor Table (GDT).
- *
- * This function configures a GDT descriptor with the specified base address,
- * limit, and access flags. The GDT is used in x86_64 architecture to define
- * the characteristics of the various memory segments used by the CPU.
- *
- * @param selector The index of the descriptor in the GDT.
- * @param base The base address of the segment.
- * @param limit The limit of the segment.
- */
-void gdt_set_descriptor(uint8_t selector, uint32_t base, uint32_t limit,
-			uint8_t access, uint8_t flags)
-{
-	segment_descriptor *descriptor = &gdt[selector / 8];
-
-	descriptor->base_low = base & 0xFFFF;
-	descriptor->base_mid = (base >> 16) & 0xFF;
-	descriptor->base_high = (base >> 24) & 0xFF;
-	descriptor->limit_low = limit & 0xFFFF;
-	descriptor->limit_high = (limit >> 16) & 0xF;
-	descriptor->access = access;
-	descriptor->flags = flags;
-}
 
 /**
  * @brief Initializes the Global Descriptor Table (GDT) for the x86_64 architecture.
@@ -61,25 +42,18 @@ void gdt_set_descriptor(uint8_t selector, uint32_t base, uint32_t limit,
  */
 void gdt_init(void)
 {
-	gdt_set_descriptor(0, 0, 0, 0, 0);
-	gdt_set_descriptor(0x8, 0, 0xFFFFF, 0x9A, 0xA);
-	gdt_set_descriptor(0x10, 0, 0xFFFFF, 0x92, 0xC);
-	gdt_set_descriptor(0x18, 0, 0xFFFFF, 0xFA, 0xA);
-	gdt_set_descriptor(0x20, 0, 0xFFFFF, 0xF2, 0xC);
-	gdt_set_descriptor(0x28, 0, 0, 0, 0);
-
 	asm volatile("lgdt %0;"
 		     "pushq $0x8;"
 		     "leaq 1f, %%rax;"
 		     "pushq %%rax;"
 		     "lretq;"
+		     "1:"
 		     "movw $0x10, %%ax;"
 		     "movw %%ax, %%ds;"
 		     "movw %%ax, %%es;"
 		     "movw %%ax, %%fs;"
 		     "movw %%ax, %%gs;"
 		     "movw %%ax, %%ss;"
-		     "1:"
 		     :
 		     : "m"(_gdtr));
 }
